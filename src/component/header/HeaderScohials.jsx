ed107536911a3d005d3439fb8fd302d7e873b135
abdo887 <abdoraga52@gmail.com>
2022-11-14 15:27:28 +0200

    editing a tags

--- src/component/header/HeaderScohials.jsx
+++ src/component/header/HeaderScohials.jsx
@@ -5,7 +5,7 @@ import {SiGmail} from "react-icons/si"
 const HeaderScohials = () => {
   return (
     <div className='header__socials'>
-  <a href='https://www.linkedin.com/in/abdelrhman-rgab-623462252/' target="_blank" rel="noreferrer"><BsLinkedin/></a>
+  <a href='https://www.linkedin.com/in/abdelrhman-ragab-623462252/' target="_blank" rel="noreferrer"><BsLinkedin/></a>
   <a href='https://github.com/abdo887' target="_blank" rel="noreferrer"><FaGithub/></a>
   <a href='https://www.FaceBook.com' target="_blank" rel="noreferrer"><SiGmail/></a>
     </div>
